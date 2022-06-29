6b30f1892c208cd7ec79c39e1330e73deacf738d
unknown <tizianamalko@gmail.com>
2022-06-29 17:11:22 +0200

    weather

---
+++ weather/script.js
@@ -0,0 +1,16 @@
+function loading(){
+    alert('The weather is changing');
+}
+
+function hide(tag){
+    document.querySelector('.Accept').remove();
+    
+}
+
+function change(){
+    
+    console.log(temperatura)
+
+}
+
+change();
